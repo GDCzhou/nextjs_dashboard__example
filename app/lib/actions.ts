cf8366e37232be7e347a7e494bce2b81aab28f60
GDCzhou <996078450@qq.com>
2024-08-27 23:59:24 +0800

    form 提交

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
  
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -13,12 +14,18 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 
 export async function createInvoice(formData: FormData) {
-
+  
   const raFormData = {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   }
-  console.log(raFormData);
-  
+  const { customerId, amount, status } = CreateInvoice.parse(formData)
+
+  const amountInCents = amount * 100; // Convert to cents
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
 }
