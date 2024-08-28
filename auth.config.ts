9aee5d4dc2e8cbfbe3ca4ec6ecbc9dacada366ca
GDCzhou <996078450@qq.com>
2024-08-28 23:55:09 +0800

    router 导航

--- auth.config.ts
+++ auth.config.ts
@@ -6,12 +6,23 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log({nextUrl});
+      
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnLogin = nextUrl.pathname.startsWith('/login')
+      const isHome = nextUrl.pathname.endsWith('/')
+      console.log({p :nextUrl.pathname,isLoggedIn,isOnDashboard,isOnLogin,});
+      
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+        // return Response.redirect(new URL('/',nextUrl ))
+      } else if (isLoggedIn && !isHome) {
+        // console.log(auth);
+        console.log('重定向到dashboard');
+        
+        
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
