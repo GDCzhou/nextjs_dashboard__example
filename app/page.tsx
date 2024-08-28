79f801171e6e9adaf430d8e6930ae8dc04084b90
GDCzhou <996078450@qq.com>
2024-08-28 19:45:08 +0800

    style: change css

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}></div>
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-center justify-between rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
+        <div className={styles.shape}></div>
       </div>
 
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
