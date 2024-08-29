05c9b2fddebef254d42511125876fac886daa0ac
GDCzhou <996078450@qq.com>
2024-08-29 13:13:40 +0800

    login form button style

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -64,8 +64,8 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+        <Button className="mt-4 w-full justify-center gap-2" aria-disabled={isPending}>
+          Log in <ArrowRightIcon className=" h-5 w-5  text-gray-50 " />
         </Button>
         <div className="flex h-8 items-end space-x-1">
         {errorMessage && (
